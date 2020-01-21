f9908ff87ac86a04994ed760c240dfc352193a98
mdekauwe <mdekauwe@gmail.com>
2020-01-21 14:49:45 +1100

    Further gadi fixes

--- global/submit_to_raijin.sh
+++ global/submit_to_raijin.sh
@@ -15,7 +15,7 @@
 #   2. In an editor, set appropriate values for the variables NPROCESS,
 #      INSFILE, GRIDLIST and OUTFILES (NB: no space after the = sign):
 
-NPROCESS=128
+NPROCESS=144
 WALLTIME=04:00:00
 MEMORY=64GB
 INSFILE=global_cru.ins
